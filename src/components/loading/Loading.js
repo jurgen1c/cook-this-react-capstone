042c9490d37a006232e3ec307630e98c106a838c
jurgen1c <jurgen1c@gmail.com>
2021-03-10 08:38:41 -0600

    Add loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div>
+      <div style={{padding:'100.000% 0 0 0', position: 'relative'}}>
+        <iframe src="https://gifer.com/embed/4V0b" width="100%" height="100%" style='position:absolute;top:0;left:0;' frameBorder="0" allowFullScreen></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
--- src/components/recipe/RecipeItem.js
+++ src/components/recipe/RecipeItem.js
@@ -1,7 +1,7 @@
 import './recipeItem.css';
 
 const RecipeItem = () => {
-  
+  const recipe = useSelector(state => state.recipeList.active)
   return (
     <div>
       <h2>This is a recipe</h2>
